4cfc9e02d27f39a7d190f2a584fd30aec4fa0cac
NasibNia <nasibnia@gmail.com>
2018-09-15 21:36:12 -0700

    login

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -1,13 +1,45 @@
 $(document).ready(function(){
     var loggedIn = false;
+    var usernameInput = $('#username-id');
+    var passwordInput = $('#password-id');
 
     $("#start").on("click", function(event){
         // event.preventDefault();
         $.get("/logIn" , function(data){
             console.log(data);
             window.location.href = "/logIn";
-        });
+        });    
     });
+
+
+    $(document).on('click' , '.login-btn' , function(event){
+        event.preventDefault();
+
+        // if one of the fields is empty
+        if (!usernameInput.val().trim().trim() || !passwordInput.val().trim().trim()) {
+            console.log("both fields are required to continue");
+            return;
+        }
+        var userInfo = {
+            name : usernameInput,
+            password:passwordInput,
+            location: "SF"
+        };
+        console.log("userInfo" , userInfo);
+
+        console.log("pressed button is " + $(this).val());
+
+        if ($(this).val() === "signIn"){
+            //go to database and 
+            
+
+
+
+        } else{
+            //do the sign Up, add the user 
+        }
+    });
+  
 
 
 
