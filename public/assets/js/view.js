c6e8681185acdb9fb81690b2e662dc01492b4833
Atton <jabusau@gmail.com>
2018-09-19 14:09:11 -0700

    fixed sign out sign in

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -197,14 +197,16 @@ function onSignIn(googleUser) {
     // userInfo.email = profile.getEmail();
 
 
-    var email = userInfo.email;
+    var email = localStorage.getItem("email");
     $.get("/api/users/", function(results){
 
         console.log(results);
         var found = false;
         for (var i = 0; i < results.length; i++){
-
+            console.log(email)
+            console.log(results[i].email)
             if (results[i].email === email) {
+
 
                 console.log("User exists")
                 found = true;
